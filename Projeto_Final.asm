c65186907bd4b82153a01f31b3bbcd5556cd7d86
David Gabriel <146039653+DedeKelpa@users.noreply.github.com>
2025-04-30 22:00:55 -0300

    Update Projeto_Final.asm
    Junção dos códigos do Display e da sequência dos LEDs

--- Projeto_Final.asm
+++ Projeto_Final.asm
@@ -1,6 +1,65 @@
-ORG 0000H
+RS      equ     P1.3    ; Reg Select ligado em P1.3
+EN      equ     P1.2    ; Enable ligado em P1.2
 
-START:
+ORG 0000H
+    LJMP MAIN
+
+ORG 0100H
+MAIN:
+    ; Inicializa LCD e exibe mensagem
+    ACALL lcd_init
+
+    ; Primeira linha: "APERT E 0"
+    MOV A, #01H
+    ACALL posicionaCursor
+    MOV A, #'A'   ; Letras da mensagem
+    ACALL sendCharacter
+    MOV A, #'P'
+    ACALL sendCharacter
+    MOV A, #'E'
+    ACALL sendCharacter
+    MOV A, #'R'
+    ACALL sendCharacter
+    MOV A, #'T'
+    ACALL sendCharacter
+    MOV A, #'E'
+    ACALL sendCharacter
+    MOV A, #' '
+    ACALL sendCharacter
+    MOV A, #'0'
+    ACALL sendCharacter
+
+    ; Segunda linha: "PARA INICIAR"
+    MOV A, #0C0H
+    ACALL posicionaCursor
+    MOV A, #'P'
+    ACALL sendCharacter
+    MOV A, #'A'
+    ACALL sendCharacter
+    MOV A, #'R'
+    ACALL sendCharacter
+    MOV A, #'A'
+    ACALL sendCharacter
+    MOV A, #' '
+    ACALL sendCharacter
+    MOV A, #'I'
+    ACALL sendCharacter
+    MOV A, #'N'
+    ACALL sendCharacter
+    MOV A, #'I'
+    ACALL sendCharacter
+    MOV A, #'C'
+    ACALL sendCharacter
+    MOV A, #'I'
+    ACALL sendCharacter
+    MOV A, #'A'
+    ACALL sendCharacter
+    MOV A, #'R'
+    ACALL sendCharacter
+
+    ACALL retornaCursor
+
+    ; Inicializa Timer
     MOV TMOD, #01H
     SETB TR0
 
@@ -38,16 +97,16 @@ MOSTRA_LED:
     MOV P1, #0FFH
     MOV P2, #0FFH
 
-MAIN:
+LEITURA_TECLADO:
     ACALL LER_TECLA
     CJNE A, #0FFH, ACENDE_LED
-    SJMP MAIN
+    SJMP LEITURA_TECLADO
 
 ACENDE_LED:
     CPL A
     MOV P1, A
     ACALL DELAY
-    SJMP MAIN
+    SJMP LEITURA_TECLADO
 
 LER_TECLA:
 ESPERA_SOLTAR:
@@ -78,5 +137,191 @@ SEQUENCIA:
     DB 11111011B
     DB 11011111B
     DB 11101111B
+    DB 01111111B
+    DB 11111101B
+    DB 11110111B
 
-END
+lcd_init:
+
+	CLR RS		; clear RS - indicates that instructions are being sent to the module
+
+; function set	
+	CLR P1.7		; |
+	CLR P1.6		; |
+	SETB P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay_lcd		; wait for BF to clear	
+					; function set sent for first time - tells module to go into 4-bit mode
+; Why is function set high nibble sent twice? See 4-bit operation on pages 39 and 42 of HD44780.pdf.
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+					; same function set high nibble sent a second time
+
+	SETB P1.7		; low nibble set (only P1.7 needed to be changed)
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+				; function set low nibble sent
+	CALL delay_lcd		; wait for BF to clear
+
+
+; entry mode set
+; set to increment with no shift
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	SETB P1.6		; |
+	SETB P1.5		; |low nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay_lcd		; wait for BF to clear
+
+
+; display on/off control
+; the display is turned on, the cursor is turned on and blinking is turned on
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	SETB P1.7		; |
+	SETB P1.6		; |
+	SETB P1.5		; |
+	SETB P1.4		; | low nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay_lcd		; wait for BF to clear
+	RET
+
+
+sendCharacter:
+	SETB RS  		; setb RS - indicates that data is being sent to module
+	MOV C, ACC.7		; |
+	MOV P1.7, C			; |
+	MOV C, ACC.6		; |
+	MOV P1.6, C			; |
+	MOV C, ACC.5		; |
+	MOV P1.5, C			; |
+	MOV C, ACC.4		; |
+	MOV P1.4, C			; | high nibble set
+
+	SETB EN			; |
+	CLR EN			; | negative edge on E
+
+	MOV C, ACC.3		; |
+	MOV P1.7, C			; |
+	MOV C, ACC.2		; |
+	MOV P1.6, C			; |
+	MOV C, ACC.1		; |
+	MOV P1.5, C			; |
+	MOV C, ACC.0		; |
+	MOV P1.4, C			; | low nibble set
+
+	SETB EN			; |
+	CLR EN			; | negative edge on E
+
+	CALL delay_lcd			; wait for BF to clear
+	RET
+
+;Posiciona o cursor na linha e coluna desejada.
+;Escreva no Acumulador o valor de endere o da linha e coluna.
+;|--------------------------------------------------------------------------------------|
+;|linha 1 | 00 | 01 | 02 | 03 | 04 |05 | 06 | 07 | 08 | 09 |0A | 0B | 0C | 0D | 0E | 0F |
+;|linha 2 | 40 | 41 | 42 | 43 | 44 |45 | 46 | 47 | 48 | 49 |4A | 4B | 4C | 4D | 4E | 4F |
+;|--------------------------------------------------------------------------------------|
+posicionaCursor:
+	CLR RS	         ; clear RS - indicates that instruction is being sent to module
+	SETB P1.7		    ; |
+	MOV C, ACC.6		; |
+	MOV P1.6, C			; |
+	MOV C, ACC.5		; |
+	MOV P1.5, C			; |
+	MOV C, ACC.4		; |
+	MOV P1.4, C			; | high nibble set
+
+	SETB EN			; |
+	CLR EN			; | negative edge on E
+
+	MOV C, ACC.3		; |
+	MOV P1.7, C			; |
+	MOV C, ACC.2		; |
+	MOV P1.6, C			; |
+	MOV C, ACC.1		; |
+	MOV P1.5, C			; |
+	MOV C, ACC.0		; |
+	MOV P1.4, C			; | low nibble set
+
+	SETB EN			; |
+	CLR EN			; | negative edge on E
+
+	CALL delay_lcd			; wait for BF to clear
+	RET
+
+
+;Retorna o cursor para primeira posi  o sem limpar o display
+retornaCursor:
+	CLR RS	      ; clear RS - indicates that instruction is being sent to module
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CLR P1.7		; |
+	CLR P1.6		; |
+	SETB P1.5		; |
+	SETB P1.4		; | low nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay_lcd		; wait for BF to clear
+	RET
+
+
+;Limpa o display
+clearDisplay:
+	CLR RS	      ; clear RS - indicates that instruction is being sent to module
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	SETB P1.4		; | low nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay_lcd		; wait for BF to clear
+	RET
+
+
+delay_lcd:
+	MOV R0, #50
+	DJNZ R0, $
+	RET
