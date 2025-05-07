652b8173e82e6d9f3ceba5ad9ae31361e4a72838
edumoreira7 <161092739+edumoreira7@users.noreply.github.com>
2025-05-07 20:53:20 -0300

    Finalização
    Verificar sequencia do usuario e mensagem de erro/acerto após sequencia.

--- Projeto_Final.asm
+++ Projeto_Final.asm
@@ -1,67 +1,50 @@
+
 RS      equ     P1.3    ; Reg Select ligado em P1.3
 EN      equ     P1.2    ; Enable ligado em P1.2
 
 ORG 0000H
     LJMP MAIN
 
+ORG 0030H
+APERTE0:
+	DB "APERTE 0"
+	DB 00H
+PARAINICIAR:
+	DB "PARA INICIAR"
+	DB 00H
+ACERTOU:
+	DB "ACERTOU"
+	DB 00H
+ERROU:
+	DB "ERROU"
+	DB 00H
+SUAVEZ:
+	DB "SUA VEZ"
+	DB 00H
+
 ORG 0100H
 MAIN:
     ; Inicializa LCD e exibe mensagem
     ACALL lcd_init
 
-    ; Primeira linha: "APERT E 0"
+    ; Inicializa Timer
+    MOV TMOD, #01H
+    SETB TR0
+
+MENSAGEM:
+; Primeira linha: "APERTE 0"
     MOV A, #01H
     ACALL posicionaCursor
-    MOV A, #'A'   ; Letras da mensagem
-    ACALL sendCharacter
-    MOV A, #'P'
-    ACALL sendCharacter
-    MOV A, #'E'
-    ACALL sendCharacter
-    MOV A, #'R'
-    ACALL sendCharacter
-    MOV A, #'T'
-    ACALL sendCharacter
-    MOV A, #'E'
-    ACALL sendCharacter
-    MOV A, #' '
-    ACALL sendCharacter
-    MOV A, #'0'
-    ACALL sendCharacter
+    MOV DPTR, #APERTE0
+	ACALL escreveStringROM
 
     ; Segunda linha: "PARA INICIAR"
     MOV A, #0C0H
     ACALL posicionaCursor
-    MOV A, #'P'
-    ACALL sendCharacter
-    MOV A, #'A'
-    ACALL sendCharacter
-    MOV A, #'R'
-    ACALL sendCharacter
-    MOV A, #'A'
-    ACALL sendCharacter
-    MOV A, #' '
-    ACALL sendCharacter
-    MOV A, #'I'
-    ACALL sendCharacter
-    MOV A, #'N'
-    ACALL sendCharacter
-    MOV A, #'I'
-    ACALL sendCharacter
-    MOV A, #'C'
-    ACALL sendCharacter
-    MOV A, #'I'
-    ACALL sendCharacter
-    MOV A, #'A'
-    ACALL sendCharacter
-    MOV A, #'R'
-    ACALL sendCharacter
+    MOV DPTR, #PARAINICIAR
+	ACALL escreveStringROM
 
     ACALL retornaCursor
-
-    ; Inicializa Timer
-    MOV TMOD, #01H
-    SETB TR0
 
 ESPERA_USUARIO:
     MOV A, P2
@@ -69,45 +52,92 @@ ESPERA_USUARIO:
     SJMP ESPERA_USUARIO
 
 CONTINUA:
+	ACALL clearDisplay
     MOV DPTR, #SEQUENCIA
-    MOV R0, #0
+    MOV R0, #30H          ; endereço base da RAM para armazenar a sequência mostrada
+    MOV R7, #0            ; contador de LEDs mostrados (0 a 4)
+    MOV R6, #0            ; índice auxiliar
 
-MOSTRA_SEQUENCIA:
-    MOV A, TL0
-    ANL A, #07H
-    CJNE A, #05H, OK
-    MOV A, #00H
-OK:
-    CJNE A, #06H, OK2
-    MOV A, #01H
-OK2:
-    CJNE A, #07H, MOSTRA_LED
-    MOV A, #02H
+GERA_SEQUENCIA:
+    ; Usa TH0 como semente aleatória
+    MOV A, TH0
+    ANL A, #00000111B     ; mascara para valores de 0 a 7
+    
+    ; Verifica se o valor é válido (0-7)
+    CJNE A, #8, VALOR_OK
+    MOV A, #0             ; se for 8, ajusta para 0
+VALOR_OK:
 
 MOSTRA_LED:
-    MOV R5, A
-    MOV A, R5
-    MOVC A, @A+DPTR
+    MOV R5, A             ; salva o índice aleatório
+    MOVC A, @A+DPTR       ; obtém o padrão do LED
+    MOV @R0, A            ; armazena na RAM (30H-34H)
+    
+    ; Mostra o LED
     MOV P1, A
-    ACALL DELAY
-
+    ACALL DELAY_200MS     ; mantém aceso por 500ms
+    
+    ; Apaga o LED
+    MOV P1, #0FFH
+    ACALL DELAY_100MS     ; pequeno intervalo entre LEDs
+    
+    ; Prepara próximo LED
     INC R0
-    CJNE R0, #5, MOSTRA_SEQUENCIA
+    INC R7
+    CJNE R7, #5, GERA_SEQUENCIA
 
+    ; Sequência completa, prepara para captura do usuário
     MOV P1, #0FFH
     MOV P2, #0FFH
+    MOV R0, #30H          ; reset do ponteiro para a sequência
 
-LEITURA_TECLADO:
+; INÍCIO DA COMPARAÇÃO
+COMPARA_USUARIO:
     ACALL LER_TECLA
-    CJNE A, #0FFH, ACENDE_LED
-    SJMP LEITURA_TECLADO
+    CJNE A, #0FFH, VERIFICA
+    SJMP COMPARA_USUARIO
 
-ACENDE_LED:
-    CPL A
-    MOV P1, A
-    ACALL DELAY
-    SJMP LEITURA_TECLADO
+VERIFICA:
+    CPL A                 ; inverte bits da tecla pressionada
+    MOV B, A              ; salva tecla do usuário em B
+    MOV A, @R0            ; carrega valor da sequência original
+    CJNE A, B, ERRO
 
+    ; Feedback visual - LED correto
+    MOV P1, B
+    ACALL DELAY_200MS
+    MOV P1, #0FFH
+
+    ; Próxima comparação
+    INC R0
+    INC R6
+    CJNE R6, #5, COMPARA_USUARIO
+
+    ; Todas as teclas conferem!
+    SJMP ACERTO
+
+ERRO:
+	ACALL clearDisplay
+    ACALL DELAY_200MS
+    MOV A, #01H
+    ACALL posicionaCursor
+    MOV DPTR, #ERROU
+	ACALL escreveStringROM
+    ACALL DELAY_200MS
+    SJMP MENSAGEM   ; Reinicia o jogo
+
+ACERTO:
+	ACALL clearDisplay
+	ACALL DELAY_200MS
+    MOV A, #01H
+    ACALL posicionaCursor
+    MOV DPTR, #ACERTOU
+   	ACALL escreveStringROM
+    ACALL DELAY_200MS
+    LJMP MENSAGEM   ; Reinicia o jogo
+
+; ==============================================
+; Rotinas de leitura de tecla
 LER_TECLA:
 ESPERA_SOLTAR:
     MOV A, P2
@@ -122,24 +152,38 @@ TECLA_PRESSIONADA:
     CPL A
     RET
 
-DELAY:
-    MOV R6, #0FFH
-DELAY_LOOP1:
-    MOV R7, #0FFH
-DELAY_LOOP2:
-    DJNZ R7, DELAY_LOOP2
-    DJNZ R6, DELAY_LOOP1
+; Delays melhor calibrados
+DELAY_500MS:
+    MOV R3, #5
+DELAY_500_LOOP:
+    ACALL DELAY_100MS
+    DJNZ R3, DELAY_500_LOOP
+    RET
+
+DELAY_200MS:
+    MOV R3, #2
+DELAY_200_LOOP:
+    ACALL DELAY_100MS
+    DJNZ R3, DELAY_200_LOOP
+    RET
+
+DELAY_100MS:
+    MOV R4, #100
+DELAY_100_LOOP:
+    MOV R5, #250
+    DJNZ R5, $
+    DJNZ R4, DELAY_100_LOOP
     RET
 
 SEQUENCIA:
-    DB 11111110B
-    DB 10111111B
-    DB 11111011B
-    DB 11011111B
-    DB 11101111B
-    DB 01111111B
-    DB 11111101B
-    DB 11110111B
+    DB 11111110B   ; LED 0
+    DB 11111101B   ; LED 1
+    DB 11111011B   ; LED 2
+    DB 11110111B   ; LED 3
+    DB 11101111B   ; LED 4
+    DB 11011111B   ; LED 5
+    DB 10111111B   ; LED 6
+    DB 01111111B   ; LED 7
 
 lcd_init:
 
@@ -319,6 +363,21 @@ clearDisplay:
 
 	CALL delay_lcd		; wait for BF to clear
 	RET
+
+escreveStringROM:
+  MOV R1, #00h
+	; Inicia a escrita da String no Display LCD
+loop:
+  MOV A, R1
+	MOVC A,@A+DPTR 	 ;lê da memória de programa
+	JZ finish		; if A is 0, then end of data has been reached - jump out of loop
+	ACALL sendCharacter	; send data in A to LCD module
+	INC R1			; point to next piece of data
+   MOV A, R1
+	JMP loop		; repeat
+finish:
+	RET
+
 
 
 delay_lcd:
